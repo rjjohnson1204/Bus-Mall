72845e9f86e86b3eee440fc4c170798d25a95a57
xgingerbreadmanx <rjjohnson1204@gmail.com>
2018-11-05 16:47:57 -0800

    first shot

--- app.js
+++ app.js
@@ -4,6 +4,13 @@
 function Product(name, src) {
 
 }
+ 
+  var items =  {[['bag' , 'img/bag.jpg'], ['banana' , 'img/banana.jpg'], ['bathroom' , 'img/bathroom.jpg'], ['boots' , 'img/boots.jpg'] ,['breakfast' , 'img/breakfast.jpg'] , ['bubblegum' , 'img/bubblegum.jpg'] , ['chair' , 'img/chair.jpg'] , ['cthulhu' , 'img/cthulhu.jpg'] , ['dog-duck' , 'img/dog-duck.jpg'] , ['dragon' , ' img/dragon.jpg'] , [ 'pen' , 'img/pen.jpg'] ,['pet-sweep' , 'img/pet-sweep.jpg'] , ['scissors' , 'img/scissors.jpg'] , ['shark' , 'img/shark.jpg'] , ['sweep' , 'img/sweep.jpg'] , ['tauntaun' , 'img/tauntaun.jpg'] , ['unicorn' , 'img/unicorn.jpg'] , ['usb' , 'img/usb.jpg'] , ['water-can' , 'img/water-can.jpg'] , ['wine-glass' , 'img/wine/-glass.jpg'] }
+
+  var items = [img/bag.jpg , img/banana.jpg , img/bathroom.jpg ,  img/boots.jpg , img/breakfast.jpg , img/bubblegum.jpg , img/chair.jpg , img/cthulhu.jpg , img/dog-duck.jpg , img/dragon.jpg , img/pen.jpg , img/pet-sweep.jpg , img/scissors.jpg ,  ]
+
+  var nameArray = ['bag' , 'banana', 'bathroom' , 'boots' , 'breakfast' , 'bubblegum' , 'chair' , 'cthulhu' , 'dog-duck' , 'dragon' , 'pen' , 'pet-sweep' , 'scissors' , 'shark' , 'sweep' , 'tauntuan' , 'unicorn' , 'usb' , 'water-can' , 'wine-glass']
+
 
 var tracker = {
   products: [],
